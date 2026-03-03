4011b640cf89762df5f45b7b310aad0118008b3c
Sarmad Matrood <sarmadmatrood12@gmail.com>
2026-03-04 01:38:01 +1300

    Add files via upload

---
+++ CP03/for-loop.js
@@ -0,0 +1,9 @@
+function Driving(age,hasLicense) {
+  let age = 20
+  let hasLicense = true
+  if (age >= 20 && hasLicense == true){ console.log('You can be the driver') }else {
+    console.log('You cant be the driver')
+  }
+}
+Driving(age,hasLicense)
+
