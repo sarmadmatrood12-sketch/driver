22d3ef6fe0cced3a47ca60048d8ab86088fd4b84
Sarmad Matrood <Sarmadmatrood12@gmail.com>
2026-03-04 01:53:47 +1300

    people

--- CP03/for-loop.js
+++ CP03/for-loop.js
@@ -1,4 +1,8 @@
-function Driving(age,hasLicense) {
+function Driving(age,hasLicense,people) {
+  people = [Bob,Mary,Sam]
+  for (i=0;i<people.length-1;i++){
+      console.log(people(i))
+  }
   let age = 20
   let hasLicense = true
   if (age >= 20 && hasLicense == true){ console.log('You can be the driver') }else {
